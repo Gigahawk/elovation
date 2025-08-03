43131888dde51447afb211f49c1e5ff4c1b834c8
Jasper Chan <jasperchan515@gmail.com>
2025-08-02 20:40:56 -0700

    env: create nix package

--- flake.nix
+++ flake.nix
@@ -16,11 +16,37 @@
           gemdir = ./.;
         };
       in {
+        packages = {
+          elovation-src = pkgs.stdenv.mkDerivation {
+            name = "elovation-src";
+            src = ./.;
+            installPhase = ''
+              mkdir -p $out/src/
+              find . -type f -exec install -Dm 755 "{}" "$out/src/{}" \;
+            '';
+          };
+          elovation-rails = pkgs.writeShellApplication {
+            name = "elovation-rails";
+            runtimeInputs = [ gems ];
+            text = ''
+              ELOVATION_SRC=${self.packages.${system}.elovation-src}/src
+              # Rails sucks ass and requires a writeable tmp dir in the root of the source directory
+              # Copy the source out to a writeable path and then execute from there
+              TMPDIR=$(mktemp -d)
+              cp -r --no-preserve=mode,ownership "$ELOVATION_SRC" "$TMPDIR/app"
+
+              pushd "$TMPDIR/app"
+              rails "$@"
+              popd
+            '';
+          };
+          default = self.packages.${system}.elovation-rails;
+        };
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            bundix
-            gems
+          buildInputs = [
+            pkgs.bundix
             pkgs.ruby
+            gems
           ];
         };
       });
